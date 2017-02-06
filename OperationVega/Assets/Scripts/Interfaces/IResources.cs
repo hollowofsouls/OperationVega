225b8ab64a59b65975ff218f77c6af099b8ee73e
Eugene Hochenedel <eugeneatschool@gmail.com>
2017-02-06 12:59:23 -0600

    Moved Resources and ReSharper Stuff
    Renamed Resource interface to IResources and moved it to the interfaces
    folder.
    Began implementing ReSharper and StyleCop into my code.

---
+++ OperationVega/Assets/Scripts/Interfaces/IResources.cs
@@ -0,0 +1,43 @@
+﻿
+namespace Assets.Scripts.Interfaces
+{
+	/// <summary>
+	/// The Resources interface.
+	/// </summary>
+	public interface IResources
+	{
+		/// <summary>
+		/// Gets or sets the count.
+		/// </summary>
+		int Count { get; set; }
+
+		/// <summary>
+		/// Gets or sets a value indicating whether renewable.
+		/// </summary>
+		bool Renewable { get; set; }
+
+		/// <summary>
+		/// Gets or sets a value indicating whether taint.
+		/// </summary>
+		bool Taint { get; set; }
+
+		/// <summary>
+		/// The refresh.
+		/// </summary>
+		/// <param name="value">
+		/// The value.
+		/// </param>
+		/// <param name="check">
+		/// The check.
+		/// </param>
+		/// <returns>
+		/// The <see cref="int"/>.
+		/// </returns>
+		int Refresh(int value, bool check);
+
+		/// <summary>
+		/// The reset.
+		/// </summary>
+		void Reset();
+	}
+}
